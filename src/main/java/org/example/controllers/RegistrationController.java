e4a147fcaadf0b61f1488a66b4db25293a464280
abbeleve <integralisint@outlook.com>
2024-06-09 03:19:15 +0800

    added controller and config :

---
+++ src/main/java/org/example/controllers/RegistrationController.java
@@ -0,0 +1,20 @@
+package org.example.controllers;
+
+import org.example.group.Roles;
+import org.example.group.Student;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class RegistrationController {
+
+    @PostMapping
+    public String registerUser(@RequestParam Integer age, @RequestParam String name, @RequestParam String surname, Model model) {
+        Student student = new Student(Roles.Noob, name, surname, age);
+
+        model.addAttribute("user", student);
+        return "registration-success";
+    }
+}
---
+++ src/main/java/org/example/controllers/ThymeleafConfig.java
@@ -0,0 +1,5 @@
+package org.example.controllers;
+
+import org.springframework.context.annotation.Bean;
+
+public class ThymeleafConfig {}
