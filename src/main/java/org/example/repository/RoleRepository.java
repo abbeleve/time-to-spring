7cf598565c99e9ec435cfd8e20543bd51225d2c2
Andrey <integralisint@outlook.com>
2024-06-12 01:30:44 +0800

    added rolerepository, added userrepository

--- src/main/java/org/example/repository/RoleRepository.java
+++ src/main/java/org/example/repository/RoleRepository.java
@@ -1,4 +1,8 @@
 package org.example.repository;
 
-public interface RoleRepository {
+import org.example.group.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+
 }
--- src/main/java/org/example/repository/UserRepository.java
+++ src/main/java/org/example/repository/UserRepository.java
@@ -1,4 +1,8 @@
 package org.example.repository;
 
-public interface UserRepository {
+import org.example.group.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<Student, Long> {
+    Student findByUsername(String username);
 }
